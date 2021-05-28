247e3ecdbe3fc56d4c37cd87426ba10d7a67777c
Piyush Shyam <piyush.d.shyam@gmail.com>
2021-05-28 13:37:40 +0530

    read data using comparison operators

--- src/app.js
+++ src/app.js
@@ -87,4 +87,42 @@ const getDocument = async () => {
  
 }
 
-getDocument();
+// getDocument();
+
+// read data using comparison operators
+
+const getDataByComparing = async () => {
+  try{
+
+    // $eq => Matches values that are equal to a specified value.
+    // const result = await Playlist.find({active : {$eq : false}});
+
+    // $gt => Matches values that are greater than a specified value.
+    // const result = await Playlist.find({videos : {$gt : 150}});
+
+    // $gte => Matches values that are greater than or equal to a specified value.
+    // const result = await Playlist.find({videos : {$gte : 150}});
+
+    // $in => Matches any of the values specified in an array.
+    // const result = await Playlist.find({ctype : {$in : ["Back End", "Database"]}});
+
+    // $nin => Matches none of the values specified in an array.
+    // const result = await Playlist.find({ctype : {$nin : ["Back End", "Database"]}});
+
+    // $ne => Matches all values that are not equal to a specified value.
+    // const result = await Playlist.find({videos : {$ne : 5}});
+
+    // $lt => Matches values that are less than a specified value.
+    // const result = await Playlist.find({videos : {$lt : 10}});
+
+    // $lte => Matches values that are less than or equal to a specified value.
+    const result = await Playlist.find({videos : {$lte : 10}}, {name:1,_id:0,videos:1});
+    console.log(result);
+  } catch(err) {
+
+    console.log(`Error : ${err}`);
+  }
+  
+}
+ 
+getDataByComparing();
